cf7d6aca048d6e12cd2ddd6c0afe17149119f4c4
sarkarrgb <sakarpartha222@gmail.com>
2025-07-21 15:46:46 +0530

    adding vercel josn

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const app = express()
 app.use(cors())
 app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
+app.get('/', (req, res) => {
+  res.send('Todo API is running ✅');
+});
+
 app.get('/get',(req, res)=>{
     TodoModel.find()
     .then(result => res.json(result))
