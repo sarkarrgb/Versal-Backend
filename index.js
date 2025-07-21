6dfc03eba4dac9e29451b36f9a772d7bba209809
sarkarrgb <sakarpartha222@gmail.com>
2025-07-21 15:34:53 +0530

    adding vercel josn

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require ('cors')
 const TodoModel = require('./Models/Todo');
+require('dotenv').config();
 
 
 
 const app = express()
 app.use(cors())
 app.use(express.json());
-mongoose.connect('mongodb+srv://sakarpartha222:7FKP2Dv9KA8UQ4Na@cluster0.yhd4aig.mongodb.net/todoApp?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect(process.env.MONGO_URI)
 app.get('/get',(req, res)=>{
     TodoModel.find()
     .then(result => res.json(result))
